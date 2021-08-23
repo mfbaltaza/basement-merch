128b6530ac1808fd9d2f8a4f16a73bb14f840a77
Juan Araujo <araujogreydiego@gmail.com>
2021-08-23 16:29:38 -0400

    Deletes Code from Product Component to build app

--- components/product/index.tsx
+++ components/product/index.tsx
@@ -4,18 +4,9 @@ import React from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Product(props: Object) {
   return (
-    <div>
-      <div className="bg-gradient-to-b from-black to-newDark border-b-4 min-width-xs max-w-lg">
-<<<<<<< HEAD
-        {/* <Image alt="Shop Item" src={props.img} /> */}
-=======
-       {/* <Image alt="Shop Item" src={props.img} /> */}
->>>>>>> 0b070d18623af2c2b15edcb825c8932e613ed42d
-      </div>
-      <div className="flex justify-between py-sm text-center">
-        <p className="text-xl font-bold">{/*Item Name*/}</p>
-        <p className="text-xl font-bold">{/*Item Price*/}</p>
-      </div>
+    <div className="flex justify-between py-sm text-center">
+      <p className="text-xl font-bold">{/*Item Name*/}</p>
+      <p className="text-xl font-bold">{/*Item Price*/}</p>
     </div>
   );
 }
