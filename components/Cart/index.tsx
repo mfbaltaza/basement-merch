cf47898a74d887af14ce01f880f7e35269c5fa6a
Juan Araujo <araujogreydiego@gmail.com>
2021-08-23 22:12:21 -0400

    :construction: Some styling over the cart

--- components/Cart/index.tsx
+++ components/Cart/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 import yourcartlarge from "@assets/yourcartlarge.svg";
+import checkout from "@assets/checkout.svg";
 
 interface Props {
   productName: String;
@@ -11,14 +12,22 @@ interface Props {
 
 const Index: React.FC<Props> = ({productName, setCartVisibility}) => {
   return (
-    <div className="bg-smoke-darkest bg-opacity-50 z-50 absolute inset-0 flex justify-end items-start">
-      <div className="bg-smoke-darkest h-modal max-w-xl max-h-96 flex">
-        <p className="font-bold text-2xl cursor-pointer" onClick={() => setCartVisibility(false)}>
-          CLOSE
-        </p>
+    <div className="bg-smoke-darkest bg-opacity-50 z-50 absolute inset-0 flex justify-end">
+      <div className="bg-smoke-darkest h-modal max-w-xl max-h-96 flex flex-wrap justify-end mr-4 items-end">
+        <div className="max-h-px pt-md">
+          <p className="font-bold text-2xl cursor-pointer" onClick={() => setCartVisibility(false)}>
+            CLOSE
+          </p>
+        </div>
         <Image alt="Your Cart" src={yourcartlarge} />
-        <h4>Basement</h4>
-        <p>Price </p>
+        <div className="border min-w-max h-24">
+          <h3 className="text-3xl font-bold">{`TOTAL: $2300`}</h3>
+        </div>
+        <div className="border h-24 flex">
+          <div className="self-center px-lg">
+            <Image alt="Checkout" src={checkout} />
+          </div>
+        </div>
       </div>
     </div>
   );
