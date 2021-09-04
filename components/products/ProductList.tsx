4e5b9e04823dcc13ffb2d7a5331abb062eaef2ea
Juan Araujo <araujogreydiego@gmail.com>
2021-09-04 02:39:37 -0400

    :art: Adds foundation code to ProductList

--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -1,5 +1,27 @@
-const ProductList = () => {
-  return <div />;
+import {Product} from "../../product/types";
+
+import ProductCard from "./ProductCard";
+
+interface Props {
+  products: Product[];
+  cartItems: Product[];
+  setCartItems: (x: any[]) => void;
+}
+
+const ProductList: React.FC<Props> = ({products, cartItems, setCartItems}) => {
+  return (
+    <div>
+      {products.length === 0 && <p className="text-4xl">Cart is Empty</p>}
+      {products.map((product: Product) => (
+        <ProductCard
+          key={product.id}
+          cartItems={cartItems}
+          product={product}
+          setCartItems={setCartItems}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
