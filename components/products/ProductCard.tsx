665762d7f8129072e1249b1a104ed04be1d26a93
Juan Araujo <araujogreydiego@gmail.com>
2021-09-04 02:16:23 -0400

    :art: Removes import React Line due to it being unnecessary and adds basic files to Product Showcase components

---
+++ components/products/ProductCard.tsx
@@ -0,0 +1,40 @@
+import {Product} from "../../product/types";
+
+interface Props {
+  product: Product;
+  cartItems: Product[];
+  setCartItems: any;
+}
+
+const ProductCard: React.FC<Props> = ({product, cartItems, setCartItems}) => {
+  const handleClick = () => {
+    () => {
+      const exist = cartItems.find((x) => x.id === product.id);
+
+      if (exist) {
+        setCartItems(
+          cartItems.map((x) => (x.id === product.id ? {...exist, qty: exist.qty + 1} : x)),
+        );
+      } else {
+        setCartItems([...cartItems, {...product, qty: 1}]);
+      }
+    };
+  };
+
+  return (
+    <div className="product-3" onClick={handleClick}>
+      <div className="cursor-pointer group relative bg-gradient-to-b from-black to-newDark border-b-4 min-width-xs max-w-lg">
+        <Image alt="Basement Cap" src={cap} />
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-56 right-7 sm:left-1/4 ">
+          <Image alt="Add To Cart" src={addToCart} />
+        </div>
+      </div>
+      <div className="flex justify-between py-sm text-center">
+        <p className="text-xl font-bold">{`${product.name}`}</p>
+        <p className="text-xl font-bold">{`$${product.price}`}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ components/products/ProductList.tsx
@@ -0,0 +1,5 @@
+const ProductList = () => {
+  return <div />;
+};
+
+export default ProductList;
