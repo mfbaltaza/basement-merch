585cf3b0e7febe7ab9a76fd9e5cb29032ec77dad
Juan Araujo <araujogreydiego@gmail.com>
2021-09-21 17:39:59 -0400

    :lipstick: Tweaks sizing over product showcase

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -27,8 +27,8 @@ const ProductCard: React.FC<Props> = ({product, cartItems, setCartItems}) => {
       }}
     >
       <div className="cursor-pointer group relative bg-gradient-to-b from-black to-newDark border-b-4 min-width-xs max-w-lg">
-        <Image alt="Basement Cap" height="400" src={product.image} width="300" />
-        <div className="opacity-0 group-hover:opacity-100 absolute bottom-56 right-7 sm:left-1/4 ">
+        <Image alt="Basement Cap" height="700" src={product.image} width="500" />
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-48 md:bottom-72 left-14 md:left-24">
           <Image alt="Add To Cart" src={addToCart} />
         </div>
       </div>
